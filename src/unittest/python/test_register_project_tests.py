b4d4181f856f0021174dad0885bc708daec47e34
preethi1444 <100589387@alumnos.uc3m.es>
2026-04-16 18:23:21 +0200

    tc_sa_13_field1_deleted: PASS

--- src/unittest/python/test_register_project_tests.py
+++ src/unittest/python/test_register_project_tests.py
@@ -155,5 +155,18 @@ class MyTestCase(unittest.TestCase):
 
         self.assertEqual(expected_msg, str(context.exception))
 
+    def test_(self):
+        """tc_sa_13: Missing PROJECT_ID should trigger 'JSON does not have expected structure'"""
+        manager = EnterpriseManager()
+        base_path = os.path.dirname(__file__)
+        test_file = os.path.join(base_path, "json_files", "tc_sa_13.json")
+
+        expected_msg = "JSON does not have expected structure"
+
+        with self.assertRaises(EnterpriseManagementException) as context:
+            manager.register_document(test_file)
+
+        self.assertEqual(expected_msg, str(context.exception))
+
 if __name__ == '__main__':
     unittest.main()

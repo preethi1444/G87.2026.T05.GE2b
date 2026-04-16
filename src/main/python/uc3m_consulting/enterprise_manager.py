b3bf89bf548e170294fb86f059dec58f04864904
preethi1444 <100589387@alumnos.uc3m.es>
2026-04-16 15:27:46 +0200

    RESTARTED: did not previously address the unit test coverage

--- src/main/python/uc3m_consulting/enterprise_manager.py
+++ src/main/python/uc3m_consulting/enterprise_manager.py
@@ -1,31 +1,9 @@
-import json
-from .project_document import ProjectDocument
-from .enterprise_management_exception import EnterpriseManagementException
+"""Module """
 
 class EnterpriseManager:
     """Class for providing the methods for managing the orders"""
     def __init__(self):
         pass
-
-    def register_document(self, file_path):
-        """Reads JSON and returns the document signature"""
-        try:
-            with open(file_path, "r", encoding="utf-8") as f:
-                data = json.load(f)
-        except FileNotFoundError:
-            raise EnterpriseManagementException("Input file not found")
-
-        """
-        except json.JSONDecodeError:
-            raise EnterpriseManagementException("file not JSON formatted")
-"""
-
-        project_id = data.get("PROJECT_ID")
-        file_name = data.get("FILENAME")
-
-        new_doc = ProjectDocument(project_id, file_name)
-
-        return new_doc.document_signature
 
     @staticmethod
     def validate_cif(cif: str):

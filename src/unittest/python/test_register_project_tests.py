54e57611a5a70e689a5965b1ad6b2d08bbcb0723
preethi1444 <100589387@alumnos.uc3m.es>
2026-04-16 14:28:57 +0200

    tc_sa_01_valid_baseline: FAIL

--- src/unittest/python/test_register_project_tests.py
+++ src/unittest/python/test_register_project_tests.py
@@ -1,13 +1,31 @@
 """class for testing the regsiter_order method"""
+from freezegun import freeze_time
+import json
 import unittest
-from uc3m_consulting import EnterpriseManager
+from uc3m_consulting import EnterpriseManager, EnterpriseManagementException, ProjectDocument
 
-class MyTestCase(unittest.TestCase):
-    """class for testing the register_order method"""
-    def test_something( self ):
-        """dummy test"""
-        self.assertEqual(True, False)
+class MyManagerTests(unittest.TestCase):
+    def setUp(self):
+        self.manager = EnterpriseManager()
 
+    @freeze_time("2026-04-16")  # Freezing time as per your Excel
+    def test_tc_sa_01_valid_baseline(self):
+        """
+        ID_TEST: tc_sa_01_valid_baseline
+        Description: Valid JSON - baseline (.pdf)
+        """
+        path = "tc_sa_01.json"
+        expected_hash = "c29b7d03db0e597a9a178318e0121697c8a2523c874190c1d5cfb352e0dcbd55"
+
+        data = {
+            "PROJECT_ID": "0123456789abcdef0123456789abcdef",
+            "FILENAME": "ABcd1234.pdf"
+        }
+        with open(path, "w", encoding="utf-8") as f:
+            json.dump(data, f)
+
+        result = self.manager.register_document(path)
+        self.assertEqual(result, expected_hash)
 
 if __name__ == '__main__':
     unittest.main()

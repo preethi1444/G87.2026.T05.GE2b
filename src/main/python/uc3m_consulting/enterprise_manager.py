a582728005dc518832619096eaee6c0b27c80922
preethi1444 <100589387@alumnos.uc3m.es>
2026-04-16 17:16:43 +0200

    tc_sa_04_json_not_json: PASS

--- src/main/python/uc3m_consulting/enterprise_manager.py
+++ src/main/python/uc3m_consulting/enterprise_manager.py
@@ -1,18 +1,18 @@
 import json
+import os
 from .enterprise_project import EnterpriseProject
 from .enterprise_management_exception import EnterpriseManagementException
 
 
 class EnterpriseManager:
     def register_document(self, file_path):
-        with open(file_path, "r", encoding="utf-8") as f:
-            content = f.read()
-
-        if not content.strip():
-            raise EnterpriseManagementException("JSON data has no valid values")
         try:
-            data = json.loads(content)
+            with open(file_path, "r", encoding="utf-8") as f:
+                data = json.load(f)
         except json.JSONDecodeError:
+            if os.path.getsize(file_path) == 0:
+                raise EnterpriseManagementException("JSON data has no valid values")
+
             raise EnterpriseManagementException("The file is not JSON formatted")
 
         project = EnterpriseProject(
@@ -23,5 +23,4 @@ class EnterpriseManager:
             starting_date="2026-04-16",
             project_budget=1000
         )
-
         return project.project_id
--- src/unittest/python/test_register_project_tests.py
+++ src/unittest/python/test_register_project_tests.py
@@ -1,14 +1,12 @@
 import unittest
 import os
 from freezegun import freeze_time
-# Import the Manager from the package
 from uc3m_consulting import EnterpriseManager, EnterpriseManagementException
 
 class MyTestCase(unittest.TestCase):
     @freeze_time("2026-04-16")
     def test_tc_sa_01_valid_baseline(self):
         manager = EnterpriseManager()
-
         base_path = os.path.dirname(__file__)
         test_file = os.path.join(base_path, "json_files", "tc_sa_01.json")
 
@@ -43,6 +41,15 @@ class MyTestCase(unittest.TestCase):
             manager.register_document(test_file)
 
         self.assertEqual(expected_msg, str(context.exception))
+
+    def test_tc_sa_04_json_not_json(self):
+        """tc_sa_04: This SHOULD fail if the manager has no try/except"""
+        manager = EnterpriseManager()
+        base_path = os.path.dirname(__file__)
+        test_file = os.path.join(base_path, "json_files", "tc_sa_04.json")
+
+        with self.assertRaises(EnterpriseManagementException):
+            manager.register_document(test_file)
 
 if __name__ == '__main__':
     unittest.main()

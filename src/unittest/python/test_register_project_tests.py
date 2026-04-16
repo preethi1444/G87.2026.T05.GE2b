8fa086b111f120f39c173b14b9d3202259d8d1c0
preethi1444 <100589387@alumnos.uc3m.es>
2026-04-16 15:08:56 +0200

    tc_sa_04_json_not_json: PASS

--- src/unittest/python/test_register_project_tests.py
+++ src/unittest/python/test_register_project_tests.py
@@ -82,22 +82,5 @@ class MyManagerTests(unittest.TestCase):
 
         self.assertEqual(str(context.exception), "file not JSON formatted")
 
-    def test_tc_sa_05_open_brace_deleted(self):
-            """
-            ID_TEST: tc_sa_05_open_brace_deleted
-            Description: <open_object> deleted: opening { missing
-            """
-            path = "tc_sa_05.json"
-            content = """ "PROJECT_ID": "0123456789abcdef0123456789abcdef",
-    "FILENAME": "ABcd1234.pdf"
-    }"""
-            with open(path, "w", encoding="utf-8") as f:
-                f.write(content)
-
-            with self.assertRaises(EnterpriseManagementException) as context:
-                self.manager.register_document(path)
-
-            self.assertEqual(str(context.exception), "file not JSON formatted")
-
 if __name__ == '__main__':
     unittest.main()

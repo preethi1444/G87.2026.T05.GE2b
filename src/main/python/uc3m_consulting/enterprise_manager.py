3c4e4857df778f67e2464e1de2799c720d60e1fe
preethi1444 <100589387@alumnos.uc3m.es>
2026-04-16 14:29:56 +0200

    tc_sa_01_valid_baseline: PASS

--- src/main/python/uc3m_consulting/enterprise_manager.py
+++ src/main/python/uc3m_consulting/enterprise_manager.py
@@ -1,9 +1,31 @@
-"""Module """
+import json
+from .project_document import ProjectDocument
+from .enterprise_management_exception import EnterpriseManagementException
 
 class EnterpriseManager:
     """Class for providing the methods for managing the orders"""
     def __init__(self):
         pass
+
+    def register_document(self, file_path):
+        """Reads JSON and returns the document signature"""
+        try:
+            with open(file_path, "r", encoding="utf-8") as f:
+                data = json.load(f)
+        except FileNotFoundError:
+            raise EnterpriseManagementException("Input file not found")
+        except json.JSONDecodeError:
+            raise EnterpriseManagementException("file not JSON formatted")
+
+        # Extract data from JSON
+        project_id = data.get("PROJECT_ID")
+        file_name = data.get("FILENAME")
+
+        # Create the document object
+        new_doc = ProjectDocument(project_id, file_name)
+
+        # Return the signature for the test assertion
+        return new_doc.document_signature
 
     @staticmethod
     def validate_cif(cif: str):

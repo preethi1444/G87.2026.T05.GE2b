5e2725a45fd1499607cc4179a3e22c8234d2a625
Sanyukta-UC3M <100589648@alumnos.uc3m.es>
2026-04-22 21:43:01 +0200

    TC 47: FAIL

--- src/unittest/python/test_register_project_tests.py
+++ src/unittest/python/test_register_project_tests.py
@@ -491,6 +491,13 @@ class MyTestCase(unittest.TestCase):
             manager.register_document(test_file)
         self.assertEqual("JSON does not have expected structure", str(context.exception))
 
+    def test_tc_sa_47_fn_value_deleted(self):
+        """tc_sa_47: <FILENAME_VALUE> deleted: only extension remains"""
+        manager = EnterpriseManager()
+        test_file = os.path.join(os.path.dirname(__file__), "json_files", "tc_sa_47.json")
+        with self.assertRaises(EnterpriseManagementException) as context:
+            manager.register_document(test_file)
+        self.assertEqual("JSON data has no valid values", str(context.exception))
 
 
 

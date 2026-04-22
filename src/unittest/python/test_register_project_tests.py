673229f59783fb141c93881768bfbd3097b5a4de
Sanyukta-UC3M <100589648@alumnos.uc3m.es>
2026-04-22 22:05:07 +0200

    TC 60: PASS

--- src/unittest/python/test_register_project_tests.py
+++ src/unittest/python/test_register_project_tests.py
@@ -591,7 +591,13 @@ class MyTestCase(unittest.TestCase):
             manager.register_document(test_file)
         self.assertEqual("JSON data has no valid values", str(context.exception))
 
-
+    def test_tc_sa_60_alphanum_space(self):
+        """tc_sa_60: Alphanum terminal: space in NAME"""
+        manager = EnterpriseManager()
+        test_file = os.path.join(os.path.dirname(__file__), "json_files", "tc_sa_60.json")
+        with self.assertRaises(EnterpriseManagementException) as context:
+            manager.register_document(test_file)
+        self.assertEqual("JSON data has no valid values", str(context.exception))
 
 
 

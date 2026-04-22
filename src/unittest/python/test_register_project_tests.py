02350c12441875c1072777643e7f76d627444e24
Sanyukta-UC3M <100589648@alumnos.uc3m.es>
2026-04-22 20:57:40 +0200

    TC 44: FAIL

--- src/unittest/python/test_register_project_tests.py
+++ src/unittest/python/test_register_project_tests.py
@@ -467,6 +467,13 @@ class MyTestCase(unittest.TestCase):
             manager.register_document(test_file)
         self.assertEqual("JSON data has no valid values", str(context.exception))
 
+    def test_tc_sa_44_hex_symbol_char(self):
+        """tc_sa_44: Hex terminal: symbol (@) in PROJECT_ID"""
+        manager = EnterpriseManager()
+        test_file = os.path.join(os.path.dirname(__file__), "json_files", "tc_sa_44.json")
+        with self.assertRaises(EnterpriseManagementException) as context:
+            manager.register_document(test_file)
+        self.assertEqual("JSON data has no valid values", str(context.exception))
 
 
 

c27071aee38ae10bafd14d7a7123468ff16ee2e4
Sanyukta-UC3M <100589648@alumnos.uc3m.es>
2026-04-22 21:58:13 +0200

    TC 52: PASS

--- src/unittest/python/test_register_project_tests.py
+++ src/unittest/python/test_register_project_tests.py
@@ -532,7 +532,13 @@ class MyTestCase(unittest.TestCase):
             manager.register_document(test_file)
         self.assertEqual("JSON data has no valid values", str(context.exception))
 
-
+    def test_tc_sa_52_ext_duplicated(self):
+        """tc_sa_52: <EXTENSION> duplicated: .pdf.pdf"""
+        manager = EnterpriseManager()
+        test_file = os.path.join(os.path.dirname(__file__), "json_files", "tc_sa_52.json")
+        with self.assertRaises(EnterpriseManagementException) as context:
+            manager.register_document(test_file)
+        self.assertEqual("JSON data has no valid values", str(context.exception))
 
 
 

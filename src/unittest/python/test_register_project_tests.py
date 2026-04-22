58a3e8541cf7549b4115c960696c6d8a326fc99e
Sanyukta-UC3M <100589648@alumnos.uc3m.es>
2026-04-22 22:01:39 +0200

    TC 56: PASS

--- src/unittest/python/test_register_project_tests.py
+++ src/unittest/python/test_register_project_tests.py
@@ -564,7 +564,13 @@ class MyTestCase(unittest.TestCase):
             manager.register_document(test_file)
         self.assertEqual("JSON data has no valid values", str(context.exception))
 
-
+    def test_tc_sa_56_alphanum_7_chars(self):
+        """tc_sa_56: Alphanum terminal: 7 chars (n-1 boundary)"""
+        manager = EnterpriseManager()
+        test_file = os.path.join(os.path.dirname(__file__), "json_files", "tc_sa_56.json")
+        with self.assertRaises(EnterpriseManagementException) as context:
+            manager.register_document(test_file)
+        self.assertEqual("JSON data has no valid values", str(context.exception))
 
 
 
